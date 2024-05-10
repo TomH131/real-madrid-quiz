f7c7eb9a329cc4668faf11195e51aa9717f3ef4f
TomH131 <twhill2010@hotmail.co.uk>
2024-05-10 14:24:39 +0000

    adding the questions and answers to JS

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        question: "Who is the all-time top scorer for Real Madrid?",
+        options: ["Cristiano Ronaldo", "Raul", "Alfredo Di Stefano", "Karim Benzema"],
+        answer: "Cristiano Ronaldo"
+    },
+    {
+        question: "How many UEFA Champions League titles has Real Madrid won?",
+        options: ["10", "12", "14", "16"],
+        answer: "14"
+    },
+    {
+        question: "Who is the current manager of Real Madrid?",
+        options: ["Zinedine Zidane", "Jose Mourinho", "Carlo Ancelotti", "Rafa Benitez"],
+        answer: "Carlo Ancelotti"
+    },
+    {
+        question: "Who is the current captain of Real Madrid?",
+        options: ["Nacho", "Sergio Ramos", "Jude Bellingham", "Luka Modric"],
+        answer: "Nacho"
+    },
+    {
+        question: "What time did Sergio Ramos score the famous equalising goal in the 2014 Champions League final?",
+        options: ["89:00", "91:54", "92:48", "90:02"],
+        answer: "92:48"
+    },
+    {
+        question: "Who is Real Madrid's most expensive transfer?",
+        options: ["Gareth Bale", "Jude Bellingham", "Eden Hazard", "Toni Kroos"],
+        answer: "Eden Hazard"
+    },
+    {
+        question: "What year was Real Madrid founded?",
+        options: ["1910", "1898", "1955", "1902"],
+        answer: "1902"
+    },
+    {
+        question: "Who is the current Real Madrid President?",
+        options: ["Ramon Calderon", "Florentino Perez", "David Beckham", "Joan Laporta"],
+        answer: "Florentino Perez"
+    }
+];
