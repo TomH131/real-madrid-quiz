dca163773faa4ebe59d0471b2573f67364e3cbcf
TomH131 <twhill2010@hotmail.co.uk>
2024-05-15 08:52:37 +0000

    adding images and inlcuding them in the JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,37 +18,44 @@ let questions = [
     {
         question: "2. How many UEFA Champions League titles has Real Madrid won?",
         answers: shuffleAnswers(["10", "12", "14", "16"]),
-        correctAnswer: "14"
+        correctAnswer: "14",
+        image: "assets/images/madrid-14.jpg"
     },
     {
         question: "3. Who is the current manager of Real Madrid?",
         answers: shuffleAnswers(["Zinedine Zidane", "Jose Mourinho", "Carlo Ancelotti", "Rafa Benitez"]),
-        correctAnswer: "Carlo Ancelotti"
+        correctAnswer: "Carlo Ancelotti",
+        image: "assets/images/carlo-ancelotti.jpg"
     },
     {
         question: "4. Who is the current captain of Real Madrid?",
         answers: shuffleAnswers(["Nacho", "Sergio Ramos", "Jude Bellingham", "Luka Modric"]),
-        correctAnswer: "Nacho"
+        correctAnswer: "Nacho",
+        image: "assets/images/nacho.jpg"
     },
     {
         question: "5. What time did Sergio Ramos score the famous equalising goal in the 2014 Champions League final?",
         answers: shuffleAnswers(["89:00", "91:54", "92:48", "90:02"]),
-        correctAnswer: "92:48"
+        correctAnswer: "92:48",
+        image: "assets/images/sergio-ramos.jpg"
     },
     {
         question: "6. Who is Real Madrid's most expensive transfer?",
         answers: shuffleAnswers(["Gareth Bale", "Jude Bellingham", "Eden Hazard", "Toni Kroos"]),
-        correctAnswer: "Eden Hazard"
+        correctAnswer: "Eden Hazard",
+        image: "assets/images/eden-hazard.jpg"
     },
     {
         question: "7. What year was Real Madrid founded?",
         answers: shuffleAnswers(["1910", "1898", "1955", "1902"]),
-        correctAnswer: "1902"
+        correctAnswer: "1902",
+        image: "assets/images/real-madrid-1902.jpg"
     },
     {
         question: "8. Who is the current Real Madrid President?",
         answers: shuffleAnswers(["Ramon Calderon", "Florentino Perez", "David Beckham", "Joan Laporta"]),
-        correctAnswer: "Florentino Perez"
+        correctAnswer: "Florentino Perez",
+        image: "assets/images/florentino-perez.jpg"
     }
 ];
 
@@ -86,7 +93,6 @@ function checkAnswer(selectedOption) {
             title: "Correct!",
             imageWidth: 425,
             imageHeight: 250,
-            imageAlt: "Cristiano Ronaldo",
             text: `You got the correct answer`,
             showConfirmButton: false,
             timer: 1500
